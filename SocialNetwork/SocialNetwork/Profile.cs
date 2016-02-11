43c0c65346c8baaa0d1c5cee2f54e530e1bba09c
sfizor <samantha.fizor@riseinteractive.com>
2016-02-11 12:29:00 -0600

    Created new code for social network

---
+++ SocialNetwork/SocialNetwork/Profile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    public class Profile
+    {
+        //public string Name {get; set;}
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public Profile(string blahblah)
+        {
+            Name = blahblah;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, my name is " + Name);
+        }
+    }
+}
